219e5bbe39baf13cf0c792a76dff88ba8fb0959e
LittleTry <ht@yuancore.com>
2018-08-12 17:53:30 +0800

    first commit

---
+++ basic.grammar/test1.py
@@ -0,0 +1,18 @@
+print("Hello,Python!")
+if True:
+    print('True')
+else:
+    print('False')
+
+item_one = 1
+item_two = 2
+item_three = 3
+total = item_one + \
+        item_two + \
+        item_three
+print(total)
+
+total1 = ['item_one', 'item_two', 'item_three',
+          'item_four', 'item_five']
+print(total1)
+
---
+++ basic.grammar/test3.py
@@ -0,0 +1,13 @@
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+x = "a"
+y = "b"
+# 换行输出
+print(x)
+print(y)
+
+print('---------')
+# 不换行输出
+print(x, end=" ")
+print(y, end=" ")
+print()
