a6267ab3fff90a9485e3051ba5b64fc9a6d2ae3a
LittleTry <ht@yuancore.com>
2018-08-14 22:34:55 +0800

    包重构

---
+++ basic/grammar/test0_keyword.py
@@ -0,0 +1,23 @@
+import keyword
+
+print(keyword.kwlist)
+
+keywords = ['False', 'None', 'True', 'and', 'as', 'assert',
+            'async', 'await', 'break', 'class', 'continue',
+            'def', 'del', 'elif', 'else', 'except', 'finally',
+            'for', 'from', 'global', 'if', 'import', 'in', 'is',
+            'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise',
+            'return', 'try', 'while', 'with', 'yield']
+
+# 第一个注释
+# 第二个注释
+
+'''
+第三注释
+第四注释
+'''
+
+"""
+第五注释
+第六注释
+"""
