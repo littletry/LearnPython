d6781d1295d585833956dc0c2d8b63ce7d6325d5
LittleTry <ht@yuancore.com>
2018-08-27 09:53:48 +0800

    python 列表

---
+++ basic/lists/test1.py
@@ -0,0 +1,7 @@
+list1 = ['physics', 'chemistry', 1997, 2000]
+list2 = [1, 2, 3, 4, 5, 6, 7]
+list3 = ["a", "b", "c", "d"]
+
+print "list1[0]: ", list1[0]
+print "list2[1:5]: ", list2[1:5]  # 输出第二位到第五位
+
---
+++ basic/lists/test2.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*-
+
+list4 = []  # 空列表
+list4.append('Google')  # 使用 append() 添加元素
+list4.append('Runoob')
+print list4
