084b5c28e076c3a87ed5bc792932de20248e3967
LittleTry <ht@yuancore.com>
2018-08-27 16:26:43 +0800

    标准输入

---
+++ basic/files/test1.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+
+str1 = raw_input("请输入：")
+print "你输入的内容是: ", str1
+
+
+"""
+range(start, stop[, step])
+    参数说明：
+        start: 计数从 start 开始。默认是从 0 开始。例如range（5）等价于range（0， 5）;
+        stop: 计数到 stop 结束，但不包括 stop。例如：range（0， 5） 是[0, 1, 2, 3, 4]没有5
+        step：步长，默认为1。例如：range（0， 5） 等价于 range(0, 5, 1)
+"""
+
+
+str2 = input("请输入：")
+print "你输入的内容是: ", str2
