0049a63bebf94cbe5aab4f5a186ba562ab5b8649
LittleTry <ht@yuancore.com>
2018-08-31 16:06:02 +0800

    单下划线、双下划线、头尾双下划线说明

--- basic/classes/test11.py
+++ basic/classes/test11.py
@@ -10,3 +10,12 @@ class Runoob:
 
 runoob = Runoob()
 print runoob._Runoob__site
+
+"""
+单下划线、双下划线、头尾双下划线说明：
+    __foo__: 定义的是特殊方法，一般是系统定义名字 ，类似 __init__() 之类的。
+
+    _foo: 以单下划线开头的表示的是 protected 类型的变量，即保护类型只能允许其本身与子类进行访问，不能用于 from module import *
+
+    __foo: 双下划线的表示的是私有类型(private)的变量, 只能是允许这个类本身进行访问了。
+"""
