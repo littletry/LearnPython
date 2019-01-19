4c78b1e7a7f92f1c0876a9b7ca0eb4b291d111d7
littletry <1109394634@qq.com>
2019-01-19 16:38:00 +0800

    type函数

---
+++ oop/test12.py
@@ -0,0 +1,9 @@
+# _*_ coding: UTF-8 _*_
+
+# 动态语言和静态语言最大的不同，就是函数和类的定义，不是编译时定义的，而是运行时动态创建的。
+
+
+class Hello(object):
+    def hello(self, name='world'):
+        print('Hello, %s.' % name)
+
---
+++ oop/test12_2.py
@@ -0,0 +1,14 @@
+# _*_ coding: UTF-8 _*_
+
+
+from test12 import Hello
+
+h = Hello()
+h.hello()
+print type(Hello)
+print type(h)
+
+
+# type()函数可以查看一个类型或变量的类型，Hello是一个class，
+# 它的类型就是type，而h是一个实例，它的类型就是class Hello。
+
